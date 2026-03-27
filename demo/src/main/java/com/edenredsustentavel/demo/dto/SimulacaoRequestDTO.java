84a2be2b8847674b1bc20d4224e769bd92b77b79
Eduardo Palhares <ehhp040207@gmail.com>
2026-03-27 11:13:04 -0300

    Atualizei o model simulacao de acordo com o front e criacao do simulacaoRequestDTO

---
+++ demo/src/main/java/com/edenredsustentavel/demo/dto/SimulacaoRequestDTO.java
@@ -0,0 +1,13 @@
+package com.edenredsustentavel.demo.dto;
+
+
+public class SimulacaoRequestDTO{
+    public int qtd_cartoes;
+    public int multiplicidade_cartoes;
+    public double taxa_reemissao;
+    public String material;
+    public String tipo_embalagem;
+    public String destino;
+    public String frequencia;
+
+}
